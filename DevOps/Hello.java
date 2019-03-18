06f1e6d98bbc038af5102182e1f15e7e68e82a00
Abc <abcd.test@wipro.com>
2019-03-18 18:57:30 +0530

    commit

--- DevOps/Hello.java
+++ DevOps/Hello.java
@@ -6,6 +6,9 @@ public class Hello {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello Sample project");
+		for(int i=0; i<10; i++){
+			System.out.println("Hello-------"+i);
+		}
 	}
 
 }
